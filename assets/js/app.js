d2ad507b5fc61fe0395d3c9b3263bcbfe0f102a7
Ian Applekamp <ieapplekamp@gmail.com>
2019-09-20 16:47:28 -0400

    All done. May add some more customed searching with gif ratings

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,28 @@
 $(document).ready(function () {
 
     //globes
-    var topics = ['Raccoon Dog', 'Fennec Fox', 'Quoll', 'Serval'];
+    var topics = ['Shiba Inu', 'Fennec Fox', 'Quoll', 'Serval'];
 
-    //function 1
+    //function 1 - Adds buttons to the page
     function initialButtons() {
         for (var i = 0; i < topics.length; i++) {
-            var a = $("<button>");
+            var a = $("<button class='btn btn-light'>");
             a.addClass('giphy');
             a.attr("src", topics[i]);
             a.text(topics[i]);
             $('#buttons').append(a);
         }
     }
-    // calls function 1
-    initialButtons();
-   
-    // function 2 ajaz
+    
+    // function 2 - mr. ajax
     function ajaxFunk() {
-        //e.preventDefault();
+
         $('#newGifs').empty();
+        
         topics = $(this).attr("src");
-        console.log(topics);
-
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=k2mFEQxVnbiikmEUFqyX8cKLOTkglRzd&q=" + topics + "&limit=15&offset=0&rating=G&lang=en"
+        
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=k2mFEQxVnbiikmEUFqyX8cKLOTkglRzd&q=" + topics + "&limit=15&offset=0&rating=R&PG&G&lang=en"
         //note the rating ^
-        // the offset can change the gifs loaded
-
 
         $.ajax({
             url: queryURL,
@@ -34,39 +30,38 @@ $(document).ready(function () {
         }).then(function (response) {
             
             var result = response.data;
-            
+            // Loops through the results and adds a bunch of things to the result
             for (var i = 0; i < result.length; i++) {
-
+                var gifDiv = $('<div class="col-3">')
                 var ratingTag = $('<p>').text('Rating: ' + result[i].rating)
                 var image = $('<img>')
-
+                // Grabs the still and animated images
                 var imgURL = result[i].images.original_still.url;
                 var gifURL = result[i].images.fixed_width.url;
-                
+                // Allows the picture to switch between still and animated
                 image.attr('data-image', imgURL);
                 image.attr('data-status', 'still');
                 image.attr("data-gif", gifURL)
                 image.attr('src', imgURL);
-
-                $('#newGifs').append(ratingTag);
-                $('#newGifs').append(image);
-
+                // Takes all those results and adds them to the page
+                $('#newGifs').append(gifDiv);
+                $(gifDiv).append(image);
+                $(gifDiv).append(ratingTag);
+                
             }
-            // function 4
+
+            // function 4 - if the user clicks on the img, change it to a gif/ vice versa
             $('img').on('click', function () {
 
                 var status = $(this).attr('data-status');
                 
                 if (status === 'still') {
-
                     var newURL = $(this).attr('data-gif');
                     $(this).attr('src', newURL);
-                    
                     $(this).attr('data-status', 'animated');
                     console.log(status);
                    
                 } else {
-                     
                     var newURL = $(this).attr('data-image');
                     $(this).attr('src', newURL);
                     $(this).attr('data-status', 'still');
@@ -80,7 +75,7 @@ $(document).ready(function () {
 
     }
 
-    // function 3
+    // function 3 - takes the users search and adds it to the globes 
     $("#find-gif").on('click', function (event) {
         event.preventDefault();
         var search = $('#gif-input').val().trim();
@@ -89,8 +84,8 @@ $(document).ready(function () {
         initialButtons();
     })
 
-    
-
+    // calls function 1
+    initialButtons();
     // calls function 2
     $(document).on('click', ".giphy", ajaxFunk);
 
